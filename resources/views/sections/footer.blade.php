eec7738adf32f8eb078aa82027ac685bf15aaa53
Yash Tapre <yashtapre77@gmail.com>
2023-11-19 22:59:02 +0530

    Basic Layout

---
+++ resources/views/sections/footer.blade.php
@@ -0,0 +1,3 @@
+<body>
+    <h3>footetr</h3>
+</body>
---
+++ resources/views/sections/header.blade.php
@@ -0,0 +1,58 @@
+<head>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        nav {
+            background-color: #34495e;
+            padding: 15px 0;
+            text-align: center;
+            width: 1000px;
+            border-radius: 35px;
+            margin-top: 25px;
+            height: 37px;
+            float: right;
+            margin-right: 100px;
+
+        }
+
+        .navbar {
+            text-decoration: none;
+            color: #ecf0f1;
+            margin: 0 15px;
+            padding: 10px 15px;
+            font-size: 16px;
+            display: inline-block;
+            border-radius: 8px;
+            transition: background-color 0.3s ease;
+        }
+        nav a:hover {
+            background-color: #1abc9c;
+            font-size: 20px;
+        }
+        .header{
+            width: 100%;
+            height: 25%;
+            top: 50%;
+            border: 1px solid black;
+        }
+
+    </style>
+</head>
+
+<body>
+<div class="header">
+    <center>
+        <nav class="">
+            <a href="home" class="navbar">Home</a>
+            <a href="about" class="navbar">About</a>
+            <a href="work" class="navbar">Work</a>
+            <a href="history" class="navbar">History </a>
+            <a href="achievements" class="navbar">Achievements</a>
+            <a href="books" class="navbar">Books</a>
+        </nav>
+    </center>
+</div>
+</body>
